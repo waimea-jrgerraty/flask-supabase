94cb771de7a4c139466b234166c2dacc0683ed94
James Gerraty <jrgerraty@waimea.school.nz>
2025-05-28 11:24:58 +1200

    old

--- app/__init__.py
+++ app/__init__.py
@@ -1,29 +1,30 @@
 from flask import Flask
 from flask import render_template
-from random import Random
-from datetime import date
+from supabase import create_client
+from dotenv import load_dotenv
+import os
+
+# Get ENV variables
+load_dotenv()
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+# Connect to our external database
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+print(supabase)
 
 app = Flask(__name__)
 
+
 @app.get("/")
 def root_home():
-    return render_template("pages/home.jinja")
+    response = supabase.table("things").select("*").order("rating", desc=True).execute()
+    records = response.data
 
+    return render_template("pages/home.jinja", things=records)
 
-@app.get("/test/")
-def root_test():
-    return render_template("pages/test.jinja")
-
-@app.get("/about/")
-def root_about():
-    return render_template("pages/about.jinja")
-
-@app.get("/random/")
-def root_random():
-    dailyRandom = Random(date.today().strftime("%Y%M%D"))
-    n = dailyRandom.randint(1, 1_000)
-    return render_template("pages/random.jinja", number=n)
 
 @app.errorhandler(404)
 def root_404(error):
-    return render_template("pages/errorCode.jinja",err=error)
+    return render_template("pages/errorCode.jinja", err=error)
